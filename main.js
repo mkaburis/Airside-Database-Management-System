1c934101ba7db3d0666d7e587c7ec0593d606567
Jose-Pablo <jmantilla1@mail.usf.edu>
2020-04-15 22:35:03 -0400

    in case we want a simpler server we can start from here

--- main.js
+++ main.js
@@ -3,3 +3,15 @@
 // COP 4710 Database Design (Spring 2020)
 // Mihail Kaburis, Jose-Pablo Mantilla, Jody Rutter
 
+const express = require("express");
+
+const PORT = 1234;
+const app = express();
+
+app.get("/hello", (req, res) => {
+    res.send("Hello world");
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
