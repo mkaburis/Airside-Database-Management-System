93fdeeb3db4f5340fa6e27e0abf5d301a2a1b091
Jose-Pablo <jmantilla1@mail.usf.edu>
2020-04-17 20:22:05 -0400

    got first query running

--- routes/passenger.js
+++ routes/passenger.js
@@ -1,31 +1,28 @@
 const { Router } = require('express');
+const db = require('../db/postgres');
 
 const router = Router();
 
-router.param('userId', (req, res, next, userId) => {
-  console.log(req);
-  req.userId = userId;
-  next();
-});
-
-router.param('flightId', (req, res, next, flightId) => {
-  req.flightId = flightId;
-  next();
-});
-
 /* GET users info. */
-router.get('/:userId', (req, res) => {
-  res.send(`Info about a specific passenger with userId ${req.userId}`);
+router.get('/:userId', async (req, res) => {
+  const { userId } = req.params;
+  const { rows } = await db.query('SELECT * FROM users WHERE userid = $1', [
+    userId
+  ]);
+  res.send(rows);
+  // res.send(`Info about a specific passenger with userId ${req.userId}`);
 });
 
 /* Get a list of user's flights */
 router.get('/:userId/flights/:flightId', (req, res) => {
-  res.send(`Info about flight ${req.flightId} for passenger ${req.userId}`);
+  const { flightId, userId } = req.params;
+  res.send(`Info about flight ${flightId} for passenger ${userId}`);
 });
 
 /* Get a list of user's flights */
 router.get('/:userId/flights', (req, res) => {
-  res.send(`Info about a all flight taken by passenger ${req.userId}`);
+  const { userId } = req.params;
+  res.send(`Info about a all flight taken by passenger ${userId}`);
 });
 
 module.exports = router;
