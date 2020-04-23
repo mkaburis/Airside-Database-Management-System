48718e57954db11ed5a7ee734d0e3c584623a357
Jose-Pablo <jmantilla1@mail.usf.edu>
2020-04-22 20:47:47 -0400

    forgot to add slash

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const employeeRouter = require('./employee');
 const loginRouter = require('./login');
 
 module.exports = (app) => {
-  app.use('api/employee', employeeRouter);
-  app.use('api/flights', flightsRouter);
-  app.use('api/passenger', passengerRouter);
-  app.use('api/auth', loginRouter);
+  app.use('/api/employee', employeeRouter);
+  app.use('/api/flights', flightsRouter);
+  app.use('/api/passenger', passengerRouter);
+  app.use('/api/auth', loginRouter);
 };
