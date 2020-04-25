28f1ec0f9a32a992b4f62a4d28853fc9a5baa0c7
Jose-Pablo <jmantilla1@mail.usf.edu>
2020-04-25 19:44:32 -0400

    edited flight serach to only show active routes

--- routes/flights.js
+++ routes/flights.js
@@ -21,11 +21,12 @@ router.get('/', async (req, res) => {
   let { flightNo, departureAirport, arrivalAirport } = req.query;
 
   let { airline, flightNum } = extractFlightNo(flightNo);
-  if (airline === null) {
+  if (airline === null && flightNum === null) {
     airline = '%';
-  }
-  if (flightNum === null) {
     flightNum = '%';
+  }
+  if ((flightNum === null && airline !== null) || (flightNum !== null && airline === null)) {
+    return res.Status(404).json({ error: 'Flight number must have airline and flight number' });
   }
   if (arrivalAirport === '') {
     arrivalAirport = '%';
@@ -45,7 +46,7 @@ router.get('/', async (req, res) => {
 
   */
   const query = 'SELECT * FROM routes AS t1 LEFT JOIN flightlogs AS t2 ON t1.routeid '
-    + '= t2.routeid  WHERE flightno like $1 AND fliesto like $2 AND fliesfrom like $3 AND airlinecode like $4'
+    + '= t2.routeid  WHERE flightno like $1 AND fliesto like $2 AND fliesfrom like $3 AND airlinecode like $4 and isactive=true'
     // + 'AND (departuretime > $5 AND arrivaltime < $6)'
     + 'ORDER BY departuretime, arrivaltime';
   const { rows } = await db.query(query, [
@@ -53,10 +54,10 @@ router.get('/', async (req, res) => {
     // flightNo, arrivalAirport, departureAirport, airline, yesterday, tommorow
   ]);
   if (rows.length < 1) {
-    res.status(404).json({ error: 'No flights found' });
-  } else {
-    res.send(rows);
+    return res.status(404).json({ error: 'No flights found' });
   }
+
+  return res.send(rows);
 });
 
 module.exports = router;
