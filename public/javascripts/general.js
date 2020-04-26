43e2cec117744bc51bad0a9a71994a51d55eb0e0
Jose-Pablo <jmantilla1@mail.usf.edu>
2020-04-26 19:31:05 -0400

    added users page

--- public/javascripts/general.js
+++ public/javascripts/general.js
@@ -10,3 +10,16 @@ function logout() {
     }
   });
 }
+
+function addEventListeners() {
+  const logOutButton = document.getElementById('logOutButton');
+
+  // defined in general.js
+  // eslint-disable-next-line no-undef
+  logOutButton.addEventListener('click', logout);
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  addEventListeners();
+}, false);
---
+++ public/javascripts/users.js
@@ -0,0 +1,37 @@
+function populateTable(data) {
+}
+
+function addUser() {
+  const username = document.getElementById('username');
+  const authRadioGroup = document.getElementById('authRadioGroup');
+  const selectedAuth = authRadioGroup.querySelector('input[name="authRadio"]:checked').value;
+  const usernameVal = username.value;
+
+  if (!username.validity.valid) {
+    return;
+  }
+
+  const url = `../api/admin/addUser?username=${usernameVal}&permission=${selectedAuth}`;
+  fetch(url, { method: 'POST' })
+    .then((response) => {
+      if (response.status === 200) {
+        username.value = '';
+        return;
+      }
+      alert('Error adding user');
+      throw response.statusText;
+    })
+    .then((response) => populateTable(response))
+    .catch((err) => console.log(err));
+}
+
+function addEventListeners() {
+  const saveButton = document.getElementById('saveButton');
+
+  saveButton.addEventListener('click', addUser);
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  addEventListeners();
+}, false);
