95914bebd9b1d02b316d84e00c083017bcdf45d7
Jose-Pablo <jmantilla1@mail.usf.edu>
2020-04-26 14:06:52 -0400

    renamed employee.js to admin.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 // routes/index.js
 const flightsRouter = require('./flights');
 const passengerRouter = require('./passenger');
-const employeeRouter = require('./employee');
+const adminRouter = require('./admin');
 const loginRouter = require('./login');
 const staffDashRouter = require('./staffDashboard');
 const pageRoutesRouter = require('./pageRoutes');
 
 module.exports = (app) => {
   app.use('/', pageRoutesRouter);
-  app.use('/api/employee', employeeRouter);
+  app.use('/api/admin', adminRouter);
   app.use('/api/flights', flightsRouter);
   app.use('/api/passenger', passengerRouter);
   app.use('/api/auth', loginRouter);
